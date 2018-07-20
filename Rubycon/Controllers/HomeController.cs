6b5af0c682871d63e7d297993f5d13d89711a439
Galin Yordanov <gal.yordanov@gmail.com>
2018-07-20 03:49:21 +0000

    All features achieved

--- Rubycon/Controllers/HomeController.cs
+++ Rubycon/Controllers/HomeController.cs
@@ -13,8 +13,11 @@ namespace Rubycon.Controllers
 {
     public class HomeController : Controller
     {
+        //initialize HTTP client for async API calls
         private static HttpClient client = new HttpClient();
 
+
+        //load homepage
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
@@ -22,11 +25,15 @@ namespace Rubycon.Controllers
             return View();
         }
 
-        public async Task<ActionResult> TrianglePost(Triangle tri)
+
+        //async method to get triangle coordinates from triangle name
+        public async Task<ActionResult> TrianglePostAsync(Triangle tri)
         {
             string name = tri.Name;
+            //retrieve triangle object
             var task = await GetCoordinatesAsync(name);
 
+            //save object values to be displayed in view
             ViewBag.TriangleName = task.Name;
             ViewBag.PointAx = task.PointA.GetValue(0);
             ViewBag.PointAy = task.PointA.GetValue(1);
@@ -38,22 +45,59 @@ namespace Rubycon.Controllers
             return View("Index");
         }
 
+
+        //async method to get triangle name from triangle coordinates
+        public async Task<ActionResult> NamePostAsync(string coordinates)
+        {
+            ViewBag.TriangleName = await GetTriangleNameAsync(coordinates);
+
+            return View("Index");
+        }
+
+
+        //async method which sends API request with appropriate parameters attached
         public async Task<Triangle> GetCoordinatesAsync(string name)
         {
             Triangle triangle = null;
+            //set URI to API + params
             string uriString = string.Format("{0}/{1}", "http://localhost:65185/api/triangle/getcoordinates", name);
 
+            //wait for client to return response
             HttpResponseMessage response = await client.GetAsync(uriString);
+            //if successful save object to be returned
             if (response.IsSuccessStatusCode)
             {
                 triangle = await response.Content.ReadAsAsync<Triangle>();
             }
+            //set generic "something went wrong" value for testing
             else
             {
                 triangle.Name = "I am Empty";
             }
 
             return triangle;
+        }
+
+
+        //async method which sends an API request to Name Calculator API
+        public async Task<string> GetTriangleNameAsync(string input)
+        {
+            string name;
+            //set URI string to take API params
+            string uriString = string.Format("{0}/{1}", "http://localhost:65185/api/triangle/gettriangle", input);
+
+            //await response
+            HttpResponseMessage response = await client.GetAsync(uriString);
+            if (response.IsSuccessStatusCode)
+            {
+                name = await response.Content.ReadAsAsync<string>();
+            }
+            else
+            {
+                name = "I didn't work.";
+            }
+
+            return name;
         }
     }
 }
--- Rubycon/Controllers/TriangleController.cs
+++ Rubycon/Controllers/TriangleController.cs
@@ -12,16 +12,19 @@ namespace Rubycon.Controllers
 {
     public class TriangleController : ApiController
     {
+        //method to get triangle coordinates based on triangle name
         [Route("api/triangle/getcoordinates/{name}")]
         [HttpGet]
         public Triangle GetCoordinates(string name)
         {
+            //generate initial point for calculations based on triangle name
             int[] InitialPoint = new int[2]
             {
                 StringConverter.GetRow(name),
                 StringConverter.GetY(name)
             };
 
+            //create new triangle object
             Triangle triangle = new Triangle
             {
                 Name = name,
@@ -33,11 +36,15 @@ namespace Rubycon.Controllers
             return triangle;
         }
 
+        //method to get triangle name from triangle coordinates
         [Route("api/triangle/gettriangle/{coordinates}")]
         [HttpGet]
         public string GetTriangle(string coordinates)
         {
+            //convert coordinates string from request into ArrayList of point coordinates
             ArrayList PointCoords = StringConverter.GetCoordinateArrayPixels(coordinates);
+
+            //convert point coordinates into triangle name
             string Name = Calculator.CalculateNamePixels(PointCoords);
 
             return Name;
