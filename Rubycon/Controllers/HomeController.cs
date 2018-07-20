b0bbeb89071e5713344b0f61718495b106046b07
Galin Yordanov <gal.yordanov@gmail.com>
2018-07-20 00:59:37 +0000

    Attempting to implement async API call through ajax form

--- Rubycon/Controllers/HomeController.cs
+++ Rubycon/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Rubycon.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +13,42 @@ namespace Rubycon.Controllers
 {
     public class HomeController : Controller
     {
+        private static HttpClient client = new HttpClient();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpGet]
+        private async void TrianglePost(Triangle tri)
+        {
+            string name = tri.Name;
+            var task = await GetCoordinatesAsync(name);
+
+            ViewBag.TriangleName = task.Name;
+            ViewBag.PointA = task.PointA;
+            ViewBag.PointB = task.PointB;
+            ViewBag.PointC = task.PointC;
+        }
+
+        async Task<Triangle> GetCoordinatesAsync(string name)
+        {
+            Triangle triangle = null;
+
+            HttpResponseMessage response = await client.GetAsync("http://localhost:65185/api/triangle/getcoordinates/{name}");
+            if (response.IsSuccessStatusCode)
+            {
+                triangle = await response.Content.ReadAsAsync<Triangle>();
+            }
+            else
+            {
+                triangle.Name = "I am Empty";
+            }
+
+            return triangle;
         }
     }
 }
